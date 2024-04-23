5c4b4341119f58065eebde7e97fd47ad3b58a867
José Manuel Castillo Calle <jose@gmail.com>
2024-04-23 17:13:39 +0200

    Calculadora suma y resta

---
+++ test/calculadora/Operacion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author JoséManuelCastilloCa
+ */
+public class Operacion {
+    private double n1;
+    private double n2;
+    public Operacion(){
+    }
+
+
+    public double suma(double n1, double n2) {
+        
+        return n1 + n2;
+    }
+
+    public  double resta(double x, double y) {
+        
+        return x-y;
+    }
+
+ 
+}
